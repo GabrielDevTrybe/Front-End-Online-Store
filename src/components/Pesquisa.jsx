3f6c7dc3b3697ad3a9332fc3207fe596f000345a
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2022-09-08 16:25:00 -0300

    Requisito 4 feito

--- src/components/Pesquisa.jsx
+++ src/components/Pesquisa.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 export default class Pesquisa extends Component {
+  constructor() {
+    super();
+    this.getCategoriesFromApi = this.getCategoriesFromApi.bind(this);
+    this.state = {
+      categories: [],
+    };
+  }
+
+  async componentDidMount() {
+    await this.getCategoriesFromApi();
+  }
+
+  async getCategoriesFromApi() {
+    const categories = await getCategories();
+    this.setState({ categories });
+  }
+
   render() {
     const { alvo } = this.props;
+    const { categories } = this.state;
     return (
       <div
         data-testid="home-initial-message"
@@ -12,6 +31,14 @@ export default class Pesquisa extends Component {
         Digite algum termo de pesquisa ou escolha uma categoria.
         <input value={ alvo } type="text" />
         <Link to="/carrinho" data-testid="shopping-cart-button">Carrinho</Link>
+        { categories.map((categorie) => (
+          <div key={ categorie.id }>
+            <label htmlFor="categorie" data-testid="category">
+              {categorie.name}
+              <input type="radio" id="categorie" />
+            </label>
+          </div>
+        ))}
       </div>
     );
   }
