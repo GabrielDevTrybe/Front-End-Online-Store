07b0db992bf639af9f85efc660f417ae080bff95
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2022-09-13 18:05:57 -0300

    Requisito 10 feito

--- src/components/Carrinho.jsx
+++ src/components/Carrinho.jsx
@@ -4,55 +4,82 @@ import PropTypes from 'prop-types';
 export default class Carrinho extends Component {
   state = {
     carrinho: [],
+    carrinhoSemRepetições: [],
   };
 
   componentDidMount() {
     const getLocalItem = localStorage.getItem('produtos');
     const carrinho = JSON.parse(getLocalItem);
     this.setState({ carrinho });
+    this.filterCarrinho(carrinho);
   }
 
-  contador = (produto) => {
-    let contador = 0;
-    const { carrinho } = this.state;
+  filterCarrinho = (carrinho) => {
+    const carroFilt = [];
+    const getObject = (obj) => JSON.stringify(obj);
     carrinho.forEach((item) => {
-      if (produto.id === item.id) {
-        contador += 1;
-        return contador;
+      const temp = carroFilt.some((i) => getObject(i.title) === getObject(item.title));
+      if (!temp) {
+        carroFilt.push(item);
       }
     });
-    return contador;
+    this.setState({ carrinhoSemRepetições: carroFilt });
   };
 
-  increaseQuantity = (produto) => {
+  contador = (produto) => {
     const { carrinho } = this.state;
-    carrinho.splice(carrinho.length, 0, produto);
-    this.setState({ carrinho });
+    const counterFilter = carrinho.filter((item) => (item.id === produto.id));
+    return counterFilter.length;
   };
 
-  decreaseQuantity = (produto) => {
+  increaseQuantity = ({ target }) => {
+    const { name } = target;
     const { carrinho } = this.state;
-    const index = carrinho.indexOf(produto);
-    const auxiliar = -1;
-    if (index > auxiliar) {
-      carrinho.splice(index, 1);
-    }
-    this.setState({ carrinho });
+    const produtoIncrementado = carrinho.find((item) => item.id === name);
+    carrinho.push(produtoIncrementado);
+    const stringified = JSON.stringify(carrinho);
+    localStorage.setItem('produtos', stringified);
+    const storageItems = localStorage.getItem('produtos');
+    const parsed = JSON.parse(storageItems);
+    this.setState({ carrinho: parsed });
   };
 
-  // removeItem = (produto) => {
+  decreaseQuantity = ({ target }) => {
+    const { carrinho } = this.state;
+    const { name } = target;
+    const produtoDecrementado = carrinho.filter((item) => item.id === name);
+    const outrosProdutos = carrinho.filter((item) => item.id !== name);
+    const produtoReduzido = produtoDecrementado.length > 1
+      ? produtoDecrementado.slice(1) : produtoDecrementado;
+    const todosProdutos = [...outrosProdutos, ...produtoReduzido];
+    const stringified = JSON.stringify(todosProdutos);
+    localStorage.setItem('produtos', stringified);
+    const storageItems = localStorage.getItem('produtos');
+    const parsed = JSON.parse(storageItems);
+    this.setState({ carrinho: parsed });
+  };
 
-  // };
+  removeItem = ({ target }) => {
+    const { name } = target;
+    const { carrinho } = this.state;
+    const outrosItems = carrinho.filter((item) => name !== item.id);
+    const stringified = JSON.stringify(outrosItems);
+    localStorage.setItem('produtos', stringified);
+    const storageItems = localStorage.getItem('produtos');
+    const parsed = JSON.parse(storageItems);
+    this.setState({ carrinho: parsed });
+    this.filterCarrinho(parsed);
+  };
 
   render() {
-    const { carrinho } = this.state;
+    const { carrinho, carrinhoSemRepetições } = this.state;
     if (carrinho === null || carrinho.length === 0) {
       return (
         <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
       );
     }
     return (
-      [carrinho.map((produto) => (
+      [carrinhoSemRepetições.map((produto) => (
         <div key={ produto.id }>
           <h3 data-testid="shopping-cart-product-name">{ produto.title }</h3>
           <img src={ produto.thumbnail } alt={ produto.title } />
@@ -61,20 +88,23 @@ export default class Carrinho extends Component {
             type="button"
             value="+"
             data-testid="product-increase-quantity"
-            onClick={ () => this.increaseQuantity(produto) }
+            onClick={ this.increaseQuantity }
+            name={ produto.id }
           />
           <p data-testid="shopping-cart-product-quantity">{ this.contador(produto) }</p>
           <input
             type="button"
             value="-"
             data-testid="product-decrease-quantity"
-            onClick={ () => this.decreaseQuantity(produto) }
+            onClick={ this.decreaseQuantity }
+            name={ produto.id }
           />
           <input
             type="button"
             value="Remover Produto"
             data-testid="remove-product"
-            onClick={ () => this.removeItem(produto) }
+            name={ produto.id }
+            onClick={ this.removeItem }
           />
         </div>
       ))]
