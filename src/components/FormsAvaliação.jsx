9f581ae752ce91f0084c421268c01e29da5a0dda
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2022-09-17 17:22:45 -0300

    requisito 11 finalizado

--- src/components/FormsAvaliação.jsx
+++ src/components/FormsAvaliação.jsx
@@ -1,102 +1,144 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 
 export default class FormsAvaliação extends Component {
   state = {
     email: '',
     textArea: '',
+    nota: '',
+    errado: false,
+    lista: [],
   };
+
+  componentDidMount() {
+    const result = localStorage.getItem(localStorage.key(0));
+    const est = JSON.parse(result);
+    // console.log(produto);
+    if (result !== null) {
+      this.setState({ lista: est });
+    }
+  }
 
   handleChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
+    }, () => {
     });
   };
 
   enviarFormulario = () => {
-    this.setState({
-      email: '',
-      textArea: '',
-    });
+    const { email, textArea, nota, lista } = this.state;
+    const { produto } = this.props;
+    console.log(email, nota);
+    if (email.length > 0 && email.includes('@') && nota.length > 0) {
+      this.setState({ errado: false });
+      const salvar = { email, textArea, nota };
+      this.setState({ lista: [...lista, salvar] }, () => {
+        localStorage.setItem((produto.id), JSON.stringify([...lista, salvar]));
+        this.setState({
+          email: '',
+          textArea: '',
+          nota: '',
+        });
+      });
+    } else {
+      this.setState({ errado: true });
+    }
   };
 
   render() {
-    const { email, textArea } = this.state;
+    const { email, textArea, errado, lista } = this.state;
     return (
-      <form>
-        <input
-          type="email"
-          data-testid="product-detail-email"
-          required
-          value={ email }
-          onChange={ this.handleChange }
-          name="email"
-        />
-        <label htmlFor="1-rating">
+      <>
+        <form>
           <input
-            type="radio"
-            name="notaDoProduto"
-            value="1"
-            data-testid="1-rating"
-            id="1-rating"
+            type="email"
+            name="email"
+            value={ email }
+            data-testid="product-detail-email"
+            onChange={ this.handleChange }
           />
-          1
-        </label>
-        <label htmlFor="2-rating">
+          <div>
+            <input
+              type="radio"
+              id="css"
+              data-testid="1-rating"
+              name="nota"
+              value="1"
+              onChange={ this.handleChange }
+            />
+            1
+            <input
+              type="radio"
+              id="css"
+              data-testid="2-rating"
+              name="nota"
+              value="2"
+              onChange={ this.handleChange }
+            />
+            2
+            <input
+              type="radio"
+              id="css"
+              data-testid="3-rating"
+              name="nota"
+              value="3"
+              onChange={ this.handleChange }
+            />
+            3
+            <input
+              type="radio"
+              id="css"
+              data-testid="4-rating"
+              name="nota"
+              value="4"
+              onChange={ this.handleChange }
+            />
+            4
+            <input
+              type="radio"
+              id="css"
+              data-testid="5-rating"
+              name="nota"
+              value="5"
+              onChange={ this.handleChange }
+            />
+            5
+          </div>
+          <textarea
+            name="textArea"
+            value={ textArea }
+            cols="30"
+            rows="10"
+            onChange={ this.handleChange }
+            data-testid="product-detail-evaluation"
+            required
+          />
           <input
-            type="radio"
-            name="notaDoProduto"
-            value="2"
-            data-testid="2-rating"
-            id="2-rating"
+            type="button"
+            value="enviar"
+            data-testid="submit-review-btn"
+            onClick={ this.enviarFormulario }
           />
-          2
-        </label>
-        <label htmlFor="3-rating">
-          <input
-            type="radio"
-            name="notaDoProduto"
-            value="3"
-            data-testid="3-rating"
-            id="3-rating"
-          />
-          3
-        </label>
-        <label htmlFor="4-rating">
-          <input
-            type="radio"
-            name="notaDoProduto"
-            value="4"
-            data-testid="4-rating"
-            id="4-rating"
-          />
-          4
-        </label>
-        <label htmlFor="5-rating">
-          <input
-            type="radio"
-            name="notaDoProduto"
-            value="5"
-            data-testid="5-rating"
-            id="5-rating"
-          />
-          5
-        </label>
-        <textarea
-          type="text"
-          data-testid="product-detail-evaluation"
-          value={ textArea }
-          onChange={ this.handleChange }
-          name="textArea"
-        />
-        <input
-          type="button"
-          value="Enviar"
-          data-testid="submit-review-btn"
-          onClick={ this.enviarFormulario }
-        />
-      </form>
+          {errado && (
+            <h1 data-testid="error-msg">Campos inválidos</h1>
+          )}
+        </form>
+        <div>
+          {lista.map((produto) => (
+            <div key={ produto.id }>
+              <p data-testid="review-card-email">{ produto.email }</p>
+              <p data-testid="review-card-evaluation">{ produto.textArea }</p>
+              <p data-testid="review-card-rating">{ produto.nota }</p>
+            </div>
+          ))}
+        </div>
+      </>
     );
   }
 }
+FormsAvaliação.propTypes = {
+  produto: propTypes,
+}.isRequired;
